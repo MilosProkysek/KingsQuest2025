6e947b30a33c0e6039b336c8ac5f1fcf8a36b09c
Milos Prokysek <milos@prokysek.com>
2025-09-24 10:56:07 +0200

    Prvni hodina - zaklad

---
+++ KingsQuest2025.Data/Room.cs
@@ -0,0 +1,23 @@
+﻿namespace KingsQuest2025.Data
+{
+    public class Room
+    {
+        public string _name;
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public  string Description { get; set; }
+
+        public void Describe()
+        {
+            Console.WriteLine($"{Name}");
+            Console.WriteLine($"{Description}");
+            Console.WriteLine("V sale jsou: Kral, Princezna");
+            Console.WriteLine("V sale se nachazi: Trun");
+            Console.WriteLine("Odsud muzes jit do: Sluj, Zbrojnice");
+        }
+    }
+}
--- KingsQuest2025/Program.cs
+++ KingsQuest2025/Program.cs
@@ -1,7 +1,41 @@
-﻿internal class Program
+﻿using KingsQuest2025.Data;
+
+namespace KingsQuest2025
 {
-    private static void Main(string[] args)
+    internal class Program
     {
-        Console.WriteLine("Hello, World!");
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Vitej statecny rytiri v hre Kraluv Ukol!\n");
+
+            //Game init
+            Room currentRoom = new Room() { 
+                Name = "Korunni sal krale Vaclava",
+                Description = "Prostorna mistnost, bohate zdobena zlatem."};
+
+            string userInputString;
+            do
+            {
+                currentRoom.Describe();
+                Console.Write("\nCo budes delat?: ");
+
+                userInputString = Console.ReadLine() ?? "";
+                string[] userInput = userInputString.Split(' ');
+
+                switch (userInput[0])
+                {
+                    case "jdi":
+                        Console.WriteLine($"Jdes do:{userInput[1]}");
+                        break;
+                    case "konec":
+                        Console.WriteLine("Sbohem statecny rytiri!");
+                        break;
+                    default:
+                        Console.WriteLine("Neznamy prikaz.");
+                        break;
+                }
+            } while (userInputString != "konec");
+
+        }
     }
 }
